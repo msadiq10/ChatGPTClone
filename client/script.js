28088a309c1fa18d09fe0175b8194560015dfed9
Mohammed Sadiq Bagalkot <mb200@hw.ac.uk>
2022-12-24 19:14:02 +0400

    deploy server

--- client/script.js
+++ client/script.js
@@ -84,7 +84,7 @@ const handleSubmit = async(e) => {
 
   // fetch data from server (bots response)
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://chatgptclone.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
